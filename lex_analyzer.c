246b1340bc7151272b57dc03895d9f8bf036cb53
Tarushi <154829682+tarushitiwary@users.noreply.github.com>
2025-02-20 08:16:39 +0530

    Update lex_analyzer.c

--- lex_analyzer.c
+++ lex_analyzer.c
@@ -1,6 +1,28 @@
+void getNextChar(){
+    currentchar = fgetc(sourceFile);
+
+    if(currentChar == '\n'){
+        currentRow++;
+        currentCol=1;
+    }
+    else{
+        currentCol++;
+    }
+}
+
+int isKeyword(char *word){
+    const char* keyword[] = {"if", "else", "main", "while"};
+    for(int i=0; i<4; i++){
+        if(strcmp(word,keyword)){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void skipWhitespace(){
     while(isspace(currentChar) && currentChar != '\n'){
-        getnextChar();
+        getNextChar();
     }
 }
 
@@ -16,29 +38,82 @@ Token getNextToken(){
         return token;
     }
 
-    //operators 
+    //operators
     if(strchr("+-*/%=<>!&|^~", currentChar)){
         token.type = OPERATOR;
-        token.value[0]=currentChar;
+        token.value[0] = currentChar;
         getNextChar();
         token.value[1]='\0';
         return token;
     }
 
     //special symbols
-    if(strchr("[]{}():;,.",currentChar)){
+    if(strchr("[]{}():;'.,", currentChar)){
         token.type = SPECIAL_SYMBOL;
         token.value[0] = currentChar;
         getNextChar();
-        token.value[1] = '\0';
+        token.value[1]='\0';
         return token;
     }
 
     //number
     if(isdigit(currentChar)){
-        int i=0;
         token.type = NUMBER;
-        while(isdigit(currentChar))
+        int i=0;
+        while(isdigit(currentChar)){
+            token.value[i++] = currentChar;
+            getNextChar();
+        }
+        token.value[i]='\0';
+        return token;
     }
 
+    //id or keyword
+    if(isalpha(currentChar) || currentChar == '_'){
+        token.type = IDENTIFIER;
+        int i=0;
+        token.value[i++] = currentChar;
+        getNextChar();
+        
+        while(isalnum(currentChar) || currentChar == '_'){
+            token.value[i++] = currentChar;
+            getNextChar();
+        }
+        token.value[i]='\0';
+        if(isKeyword(token.value)){
+            token.type = KEYWORD;
+            addToSymbolTable(token.value, token.type);
+        }
+        else{
+            token.type = IDENTIFIER;
+            addToSymbolTable(token.value, token.type);
+        }
+        return token;
+    }
+
+    //string literal
+    if(currentChar == '"' || currentChar == '\''){
+        char quoteType = currentChar;
+        token.type = STRING_LITERAL;
+        int i=0;
+        token.value[i++] = currentChar;
+        getNextChar();
+
+        while(currentChar != quoteType && currentChar != EOF){
+            token.value[i++] = currentChar;
+            getNextChar();
+        }
+        if(currentChar == quoteType){
+            token.value[i++] = currentChar;
+            getNextChar();
+        }
+        token.value[i]='\0';
+        return token;
+    }
+
+    token.type = UNKNOWN;
+    token.value[0] = currentChar;
+    token.value[1] = '\0';
+    getNextChar();
+    return token;
 }
